f3d94e6a0e6fcbad38db969b35bd5818928589ab
Evgenij Tretyakoff <evgentretyakoff@gmail.com>
2023-12-10 11:32:15 +0000

    rename main file

--- src/main.py
+++ src/main.py
@@ -3,15 +3,6 @@ import uvicorn
 from fastapi import FastAPI
 
 
-# async def create_app():
-#     """Fabric"""
-#     app: FastAPI = FastAPI(
-#         debug=True,
-#         docs_url='/api/docs',
-#         title='Working point #3'
-#     )
-#     return app
-
 app = FastAPI(
     docs_url='/api/docs',
     title='Working point #3'
@@ -24,7 +15,6 @@ async def root() -> dict:
     return {
         "message": "this is the root directory"
     }
-    # return RedirectResponse(url="/event/")
 
 if __name__ == '__main__':
     uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
