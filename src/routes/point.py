4d9756b150285f60f8329298fa557ee9b342a2b6
Tretyak0ff <evgentretyakoff@gmail.com>
2023-12-12 15:03:09 +0300

    refactoring

--- src/routes/point.py
+++ src/routes/point.py
@@ -27,24 +27,21 @@ async def get_point(request: Request):
     #            {'x': 88.90, 'y': 2.48},
     #            {'x': 90.90, 'y': 2.58}]
 
-    x_axis = [1.00, 1.63, 1.78, 1.88, 2.00, 2.48, 2.58]
+    compression = [
+        1.00, 1.63, 1.78, 1.88, 2.00, 2.48, 2.58]
 
-    graph_1 = [
-        0.00, 22.11, 27.79, 32.31, 37.41, 64.94, 70.74
-    ]
+    surge_line = [
+        0.00, 22.11, 27.79, 32.31, 37.41, 64.94, 70.74]
 
-    graph_2 = [
-        10.00, 32.11, 37.79, 42.31, 47.41, 74.57, 80.74
-    ]
+    anti_surge_line = [
+        10.00, 32.11, 37.79, 42.31, 47.41, 74.57, 80.74]
 
     return templates.TemplateResponse(
         "point.html", {
             "request": request,
-            "x_axis": x_axis,
-            "graph_1": graph_1,
-            "graph_2": graph_2,
-            # "graph_1": graph_1,
-            # "graph_2": graph_2,
+            "y_axis": compression,
+            "line_1": surge_line,
+            "line_2": anti_surge_line,
             "message": "this is the point_router directory"
         }
     )
