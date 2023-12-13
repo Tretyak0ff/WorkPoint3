d7e18c9f5315e0f299ca0c143c5c8b20b81f817d
Tretyak0ff <evgentretyakoff@gmail.com>
2023-12-13 16:35:24 +0300

    add static js

--- src/routes/point.py
+++ src/routes/point.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 point_router = APIRouter(
     tags=["Point"]
@@ -9,24 +11,8 @@ templates = Jinja2Templates(directory="src/templates/")
 
 
 @point_router.get("/")
-async def get_point(request: Request):
-    """router get todo"""
-    # graph_1 = [{'x': 0.00, 'y': 1.00},
-    #            {'x': 25.11, 'y': 1.63},
-    #            {'x': 37.44, 'y': 1.78},
-    #            {'x': 42.88, 'y': 1.88},
-    #            {'x': 47.77, 'y': 2.00},
-    #            {'x': 78.90, 'y': 2.48},
-    #            {'x': 80.90, 'y': 2.58}]
-
-    # graph_2 = [{'x': 10.00, 'y': 1.00},
-    #            {'x': 35.11, 'y': 1.63},
-    #            {'x': 47.44, 'y': 1.78},
-    #            {'x': 52.88, 'y': 1.88},
-    #            {'x': 57.77, 'y': 2.00},
-    #            {'x': 88.90, 'y': 2.48},
-    #            {'x': 90.90, 'y': 2.58}]
-
+async def get_point(request: Request, response_class=HTMLResponse):
+    """router get /point"""
     compression = [
         1.00, 1.63, 1.78, 1.88, 2.00, 2.48, 2.58]
 
@@ -42,10 +28,5 @@ async def get_point(request: Request):
             "y_axis": compression,
             "line_1": surge_line,
             "line_2": anti_surge_line,
-            "message": "this is the point_router directory"
         }
     )
-    # return templates.TemplateResponse("todo.html", {
-    #     "request": request,
-    #     "todos": todo_list
-    # })
