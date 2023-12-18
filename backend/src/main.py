c4acf6ee21b6dd0eeb5a0ee0bff3cb21d56e3d0e
Tretyak0ff <evgentretyakoff@gmail.com>
2023-12-18 22:16:30 +0300

    refactoring

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,25 +1,26 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # NEW
+from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
+def create():
 
-# NEW
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app = FastAPI(
+        debug=True,
+        title="#1"
+    )
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:8080"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-@app.get("/")
-def home() -> dict:
-    return {
-        "message": "this is the root directory"
-    }
+    @app.get("/")
+    def home() -> dict:
+        return {
+            "message": "this is the root directory"
+        }
 
-
-# if __name__ == '__main__':
-#     run("main:app", host="0.0.0.0", port=8080, reload=True)
+    return app
