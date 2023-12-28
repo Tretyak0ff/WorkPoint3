de6a9c5f021d2c3c5adf9ee7ff31ede6086a1984
Tretyak0ff <evgentretyakoff@gmail.com>
2023-12-28 11:53:00 +0300

    refactoring config

--- backend/src/config/__init__.py
+++ backend/src/config/__init__.py
@@ -1,87 +1,8 @@
 from pathlib import Path
-from typing import Literal
 from loguru import logger
-from pydantic import ConfigDict, BaseModel
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
-# BaseConfig, BaseModel, BaseSettings
 
-
-# class PublicApiSettings(BaseModel):
-#     """Configure public API settings."""
-
-#     name: str = "Backend"
-#     urls: APIUrlsSettings = APIUrlsSettings()
-
-
-# # Database Settings
-# class DatabaseSettings(BaseModel):
-#     name: str = "db.sqlite3"
-
-#     @property
-#     def url(self) -> str:
-#         return f"sqlite+aiosqlite:///./{self.name}"
-
-
-# class KafkaSettings(BaseModel):
-#     bootstrap_servers: str = "localhost:9092"
-
-
-# # Logging Settings
-# class LoggingSettings(BaseModel):
-#     """Configure the logging engine."""
-
-#     # The time field can be formatted using more human-friendly tokens.
-#     # These constitute a subset of the one used by the Pendulum library
-#     # https://pendulum.eustace.io/docs/#tokens
-#     format: str = "{time:YYYY-MM-DD HH:mm:ss} | {level: <5} | {message}"
-
-#     # The .log filename
-#     file: str = "backend"
-
-#     # The .log file Rotation
-#     rotation: str = "1MB"
-
-#     # The type of compression
-#     compression: str = "zip"
-
-
-# class AccessTokenSettings(BaseModel):
-#     secret_key: str = "invaliad"
-#     ttl: int = 100  # seconds
-
-
-# class RefreshTokenSettings(BaseModel):
-#     secret_key: str = "invaliad"
-#     ttl: int = 100  # seconds
-
-
-# class AuthenticationSettings(BaseModel):
-#     access_token: AccessTokenSettings = AccessTokenSettings()
-#     refresh_token: RefreshTokenSettings = RefreshTokenSettings()
-#     algorithm: str = "HS256"
-#     scheme: str = "Bearer"
-
-
-# # Settings are powered by pydantic
-# # https://pydantic-docs.helpmanual.io/usage/settings/
-# class Settings(BaseSettings):
-#     debug: bool = True
-
-#     # Project file system
-#     root_dir: Path
-#     src_dir: Path
-
-#     # Infrastructure settings
-#     database: DatabaseSettings = DatabaseSettings()
-
-#     # Application configuration
-#     public_api: PublicApiSettings = PublicApiSettings()
-#     logging: LoggingSettings = LoggingSettings()
-#     authentication: AuthenticationSettings = AuthenticationSettings()
-
-#     class Config(BaseConfig):
-#         env_nested_delimiter: str = "__"
-#         env_file: str = ".env"
 
 class APIUrlsSettings(BaseModel):
     docs_url: str = "/docs"
@@ -115,6 +36,8 @@ class Settings(BaseSettings):
         env_file=".env",
         env_nested_delimiter="__",
     )
+
+    public_api: PublicApiSettings = PublicApiSettings()
 
 
 ROOT_PATH = Path(__file__).parent.parent
